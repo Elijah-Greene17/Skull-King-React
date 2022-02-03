6620975c8405d38a527684092cf39925c0365ad1
Elijah Greene <egreene017@gmail.com>
2022-02-03 07:02:37 -0500

    Played with Fetch some more

--- skull-king/src/components/CreateJoinPage/CreateJoinPage.js
+++ skull-king/src/components/CreateJoinPage/CreateJoinPage.js
@@ -1,18 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from '../../Contexts/AppContext';
 import Button from '../UI/Button/Button';
 import ApplicationInput from '../UI/Input/ApplicationInput';
 import MainView from '../UI/MainView/MainView';
 import TitleHeader from '../UI/TitleHeader/TitleHeader';
 import styles from './CreateJoinPage.module.css';
-import socket from '../../Socket/Socket';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import socket from '../../Socket/Socket';
+
+toast.configure();
 
 const CreateJoinPage = () => {
-    const { name } = useContext(AppContext);
+    const { name, showError } = useContext(AppContext);
 
-    const ping = (msg) => {
-        socket.emit('pingSocket', msg);
-    };
+    const gameId = 'A1B2';
+
+    // const ping = (msg) => {
+    //     socket.emit('pingSocket', msg);
+    // };
 
     return (
         <MainView>
@@ -20,18 +26,19 @@ const CreateJoinPage = () => {
                 <TitleHeader>Ahoy, {name}!</TitleHeader>
                 <ApplicationInput>Enter a Server Code</ApplicationInput>
                 <div>
+                    <Button onClick={() => {}}>CREATE GAME</Button>
                     <Button
                         onClick={() => {
-                            fetch('http://localhost:3001/idExists')
-                                .then((res) => res.json())
+                            fetch(`http://localhost:3001/idExists${gameId}`)
+                                .then((res) => {
+                                    if (res.ok) {
+                                        console.log('ID exists!');
+                                        res.json();
+                                    } else {
+                                        showError('This Id does not exist');
+                                    }
+                                })
                                 .then((data) => console.log(data));
-                        }}
-                    >
-                        CREATE GAME
-                    </Button>
-                    <Button
-                        onClick={() => {
-                            ping('Jellybeans');
                         }}
                     >
                         JOIN GAME
